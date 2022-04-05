2b1f03d85144f64734ba75cff5fc891cc7951781
adria <littleivanitsky@gmail.com>
2022-04-05 15:35:32 -0700

    floundering

--- index.js
+++ index.js
@@ -10,3 +10,12 @@ class BinaryTreeNode {
     this.left.add(node);
   }
 }
+
+const B = new BinaryTreeNode('B');
+const A = new BinaryTreeNode('A');
+const C = new BinaryTreeNode('C');
+const D = new BinaryTreeNode('D');
+
+B.add(A);
+B.add(D);
+B.add(C);
